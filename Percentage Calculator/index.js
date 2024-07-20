c903995de91d0843708d4a24a76c3c04b0ee41cf
Mohd Asad Ansari <asad.as9458@gmail.com>
2024-07-20 19:31:42 +0530

    Comments

--- Percentage Calculator/index.js
+++ Percentage Calculator/index.js
@@ -5,18 +5,18 @@ const hin = document.querySelector('.hin');
 const sci = document.querySelector('.sci');
 const com = document.querySelector('.com');
 const btext = document.querySelector('.text');
+
 btn.addEventListener('click', function() {
     let engl = parseInt(eng.value);
     let math = parseInt(maths.value);
     let hind = parseInt(hin.value);
     let scie = parseInt(sci.value);
     let comp = parseInt(com.value);
-    if(engl == "" || math == " " || hind == NaN || scie == NaN || comp == NaN)
-        btext.innerText = "Please Enter marks in all subject";
-    else
-    {
+
+    if (isNaN(engl) || isNaN(math) || isNaN(hind) || isNaN(scie) || isNaN(comp)) {
+        btext.innerText = "Please Enter marks in all subjects";
+    } else {
         let per = (engl + math + hind + scie + comp) / 5;
         btext.innerText = `Your Percentage is ${(per).toFixed(2)} %`;
     }
-})
-
+});
